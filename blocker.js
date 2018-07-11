868ba98aec598cd3270957f1907e6b754bb8e913
magomedovgh@gmail.com <magomedovgh@gmail.com>
2018-07-11 14:00:11 +0300

    initial commit

---
+++ blocker.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Smotert-anime add blocker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Turns off smotret-anime adds
+// @author       Gjmrd
+// @match        https://smotret-anime.ru/*
+// @include      https://smotret-anime.ru/*
+// @include      https://shikimori.org/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var watchedVideoToday = getCookieItem("watchedVideoToday");
+    var expireTime = (new Date()).setTime((new Date()).getTime() + (1000 * 60 * 60)); /* time in ms */
+    setCookieItem('watchedVideoToday', +watchedVideoToday+1, '/');
+    setCookieItem('watchedPromoVideo', (new Date()).getTime(), expireTime, '/');
+})();
