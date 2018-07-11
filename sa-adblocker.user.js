56720f78ce0e652c2df608beeab170feffd26adc
magomedovgh@gmail.com <magomedovgh@gmail.com>
2018-07-11 17:14:50 +0300

    some changes

--- sa-adblocker.user.js
+++ sa-adblocker.user.js
@@ -1,17 +1,35 @@
 // ==UserScript==
-// @name         Smotret-anime adblocker
+// @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.1
 // @description  Turns off smotret-anime adds
 // @author       Gjmrd
 // @match        https://smotret-anime.ru/*
 // @include      https://smotret-anime.ru/*
+// @include      https://play.shikimori.org/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    var watchedVideoToday = getCookieItem("watchedVideoToday");
-    var expireTime = (new Date()).setTime((new Date()).getTime() + (1000 * 60 * 60)); /* time in ms */
-    setCookieItem('watchedVideoToday', +watchedVideoToday+1, '/');
-    setCookieItem('watchedPromoVideo', (new Date()).getTime(), expireTime, '/');
+    console.log(213);
+        var script = document.createElement('script');
+        script.innerHTML = `
+           function getCookie(name) {
+
+                var matches = document.cookie.match(new RegExp(
+                     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+                ))
+           return matches ? decodeURIComponent(matches[1]) : undefined
+           }
+
+            var watchedVideoToday = getCookie("watchedVideoToday");
+            if (isNaN(watchedVideoToday))
+                watchedVideoToday = "1";
+            console.log(document.cookie);
+            var expireTime = (new Date()).setTime((new Date()).getTime() + (1000 * 60 * 60));
+            setCookieItem('watchedVideoToday', +watchedVideoToday+1, '/');
+            setCookieItem('watchedPromoVideo', (new Date()).getTime(), expireTime, '/', '');
+        `;
+        script.setAttribute('id', 'super_id_name');
+        document.body.appendChild(script);
 })();
